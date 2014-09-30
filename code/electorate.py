9b42da0c2220866d1470ffad2f596fb2202c6d9a
Claire Diehl <claire.diehl@students.olin.edu>
2014-09-30 11:48:32 -0400

    Haven't pushed in awhile. Various bits of code.

--- code/electorate.py
+++ code/electorate.py
@@ -21,9 +21,18 @@ class Electorate(thinkbayes2.Suite):
         hypo: 
         data: 
         """
-        like = 1
+        a_hypo=hypo
+        mean,std,measurement=data
+        e_hypo=measurement-a_hypo
+        like=thinkbayes2.EvalNormalPdf(e_hypo,mean,std)
         return like
 
+    def ProbLose(self):
+        total=0
+        for value,prob in self.Items():
+            if value<50:
+                total+=prob
+        return total
 
 def main():
     hypos = numpy.linspace(0, 100, 101)
@@ -35,6 +44,23 @@ def main():
     suite.Update(data)
 
     thinkplot.Pdf(suite, label='posterior')
+
+    print ('Poll 1')
+    print (suite.Mean())
+    print (suite.Std())
+    print (suite.ProbLose())
+    print (suite.ProbLess(50))
+
+    data = -2.3,4.1,49
+    suite.Update(data)
+
+    thinkplot.Pdf(suite, label='posterior2')
+
+    print ('Poll 2')
+    print (suite.Mean())
+    print (suite.Std())
+    print (suite.ProbLess(50))
+
     thinkplot.Show()
 
 
